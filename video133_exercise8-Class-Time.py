517ac639161fe90466d687ab5b27943255990975
marina04-dev <marinachatzeephraimidou@gmail.com>
2025-01-21 12:54:40 +0200

    Create video133_exercise8-Class-Time.py

---
+++ video133_exercise8-Class-Time.py
@@ -0,0 +1,51 @@
+# Time Class: Exercise 8 
+class Time:
+    def __init__(self, hour, minute, second):
+        self.hour = self.__validate(hour, 0, 23)
+        self.minute = self.__validate(minute, 0, 59)
+        self.second = self.__validate(second, 0, 59)
+        
+    def set_hour(self,hour):
+        self.hour = self.__validate(hour, 0, 23) 
+        
+    def set_minute(self,minute):
+        self.minute = self.__validate(minute, 0, 59) 
+        
+    def set_second(self,second):
+        self.second = self.__validate(second, 0, 59) 
+        
+    def __validate(self, val, low, upp):
+        if low <= val <= upp:
+            return val 
+        else:
+            return 0
+        
+    def total_seconds(self):
+        return self.hour * 3600 + self.minute * 60 + self.second
+    
+    def print_(self):
+        print(f"{str(self.hour).zfill(2)} : {str(self.minute).zfill(2)} : {str(self.second).zfill(2)}")
+
+    def next_second(self):
+        second = self.second + 1 
+        carry = second // 60
+        second = second % 60 
+        
+        minute = self.minute + carry
+        carry = minute // 60 
+        minute = minute % 60 
+        
+        hour = (self.hour + carry) % 24
+        return Time(hour, minute, second)
+
+
+t = Time(1, 45,0)
+t.print_()
+t1 = Time(11, 12, 59)
+t1.next_second().print_()
+
+t2 = Time(10, 59, 59)
+t2.next_second().print_()
+
+t3 = Time(11, 59, 59)
+t3.next_second().print_()
