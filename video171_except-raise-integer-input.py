eed510470e30a93b070809b7a2737fd21d27e09b
marina04-dev <marinachatzeephraimidou@gmail.com>
2025-02-16 09:10:32 -0800

    Update video171_except-raise-integer-input.py

--- video171_except-raise-integer-input.py
+++ video171_except-raise-integer-input.py
@@ -1,18 +1,27 @@
 def get_integer():
     while True:
         try:
-            integer = input("Enter an integer number: ").strip()
-            if integer == "":
-                raise ValueError("No Digits Entered")
-            elif not integer.isdigit():
-                raise ValueError("Wrong Input! Only Digits Please!")
-            new_integer = int(integer)
+            user_input = input("Enter an integer number: ").strip()
+            if user_input[0] == "-":
+                st = user_input[1:]
+                if st == "":
+                    raise ValueError("No Digits Entered")
+                elif not st.isdigit():
+                    raise ValueError("Wrong Input! Only Digits Please!")
+                x = -int(st)
+            else:
+                st = user_input
+                if st == "":
+                    raise ValueError("No Digits Entered")
+                elif not st.isdigit():
+                    raise ValueError("Wrong Input! Only Digits Please!")
+                x = int(st)
         except ValueError as v:
             print(v)
         except Exception as e:
             print(e)
         else:
-            print(f"The Input Integer  is: {new_integer}") 
+            print(f"The Input Integer  is: {x}") 
             break 
             
 get_integer()
