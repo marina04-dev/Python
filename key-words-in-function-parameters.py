0e88c6179f9243a032c0b41baf94b031a813630e
marina04-dev <marinachatzeephraimidou@gmail.com>
2025-03-22 11:12:29 -0700

    Delete key-words-in-function-parameters.py

--- key-words-in-function-parameters.py
+++
@@ -1,7 +0,0 @@
-def address(*,address, number, zipcode, city):
-    print(f"""Address: {address},
-Number: {number},
-Zipcode: {zipcode},
-City: {city}""")
-    
-home = address("Karamanli", "2", "36554", "Athens")
